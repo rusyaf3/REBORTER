546492a9881ef9e6a3af8542b3d1b12ffe92acfe
GOIUAL <98695572+GOIUAL@users.noreply.github.com>
2022-03-26 09:09:35 +0200

    Update بروفايل وقتي.py

--- userbot/plugins/بروفايل وقتي.py
+++ userbot/plugins/بروفايل وقتي.py
@@ -31,7 +31,7 @@ autopic_path = os.path.join(os.getcwd(), "userbot", "original_pic.png")
 digitalpic_path = os.path.join(os.getcwd(), "userbot", "digital_pic.png")
 autophoto_path = os.path.join(os.getcwd(), "userbot", "photo_pfp.png")
 
-digitalpfp = Config.DIGITAL_PIC or "https://telegra.ph/file/63a826d5e5f0003e006a0.jpg"
+digitalpfp = Config.DIGITAL_PIC or "https://telegra.ph/file/32834663acd08f758b05c.jpg"
 RR7PP = Config.TIME_JM or ""
 
 normzltext = "1234567890"
