ee73c7845d27e5dc7e5fe868c5c1402bfd48a65f
rusyaf3 <102418483+rusyaf3@users.noreply.github.com>
2022-03-28 18:18:28 +0200

    Add files via upload

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -5,5 +5,5 @@ from userbot.utils import admin_cmd
 async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit(
-            "⌯︙**مـرحبا بـك عزيـزي** \n⌯︙رابط التنصيب - [اضغط هنا](https://heroku.com/deploy?template=https://github.com/SelVa12345/Assistant)\n⌯︙قناة السورس -https://t.me/SU_SELVA"
+            "⌯︙**مـرحبا بـك عزيـزي** \n⌯︙رابط التنصيب - [اضغط هنا](https://heroku.com/deploy?template=https://github.com/SelVa12345/Assistant)\n⌯︙قناة السورس -https://t.me/RBBOU"
         )
